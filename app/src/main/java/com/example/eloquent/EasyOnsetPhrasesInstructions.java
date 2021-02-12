64fa105efb70b54bf230e99352a88204966c90c7
Lujain <LujainOthman.cs@gmail.com>
2021-02-13 00:43:12 +0300

    EasyOnset new pages

---
+++ app/src/main/java/com/example/eloquent/EasyOnsetPhrasesInstructions.java
@@ -0,0 +1,45 @@
+package com.example.eloquent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EasyOnsetPhrasesInstructions extends AppCompatActivity {
+    // start button
+    TextView textView;
+    // previous button
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_onset_phrases_instructions);
+
+        //cardViewWord button
+        //when user click on cardViewWord button  this code will move them to the EasyOnsetBreathing page
+        textView = findViewById(R.id.textViewStart);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(EasyOnsetPhrasesInstructions.this, EasyOnsetPhrases.class);
+                startActivity(intent);
+            }
+        });
+
+        //prev
+        //when user click on previous button this code will move them to the previous page
+        imageView = findViewById(R.id.imageView3Previous);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(EasyOnsetPhrasesInstructions.this, EasyOnsetPhrasesBreathing.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
