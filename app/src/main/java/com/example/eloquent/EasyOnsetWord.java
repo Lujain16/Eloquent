05e5fd4d0c030fa746cd5a1585d5a73a5e59e572
Lujain <LujainOthman.cs@gmail.com>
2021-03-21 16:42:27 +0300

    changes

--- app/src/main/java/com/example/eloquent/EasyOnsetWord.java
+++ app/src/main/java/com/example/eloquent/EasyOnsetWord.java
@@ -42,8 +42,6 @@ public class EasyOnsetWord extends AppCompatActivity {
     String category ="word";
     String result;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
